bce5d99533dbde23ddd1b15187ab887d758ad529
andyaro3 <andyaro3@aol.com>
2016-11-03 17:39:50 -0400

    heroku

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ var hbs = require('express-handlebars')
 var mongoose = require('mongoose')
 // RUN mongod in CMD:  "C:\Program Files\MongoDB\Server\3.2\bin\mongod.exe"
 
+// REMOTE mongoose db
+// HEROKU: Created mongolab-tetrahedral-46998 as MONGODB_URI
+// HEROKU URI: MONGODB_URI: mongodb://heroku_0g72l7cg:cumf2najkembkfekimftjv7e2h@ds143767.mlab.com:43767/heroku_0g72l7cg
+var uristring = "mongodb://heroku_0g72l7cg:cumf2najkembkfekimftjv7e2h@ds143767.mlab.com:43767/heroku_0g72l7cg"
+	mongoose.connect(uristring, function (err, response) {
+		if (err) {
+			console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+		} else {
+			console.log ('Succeeded connected to: ' + uristring);
+		}
+	});
+
+// LOCAL mongoose
+// mongoose.connect('mongodb://localhost:27017/hp')
+
 // Apps to intregrate passport 
 var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy
@@ -44,9 +59,6 @@ passport.deserializeUser( User.deserializeUser() )
 //   saveUninitialized: false
 // }))
 
-// mongoose
-mongoose.connect('mongodb://localhost:27017/hp')
-
 //Routes
 app.use( routes )
 
@@ -63,6 +75,9 @@ app.use(function( request, response, next ) {
 })
 
 
-app.listen(3000, function(){
-	console.log("hp final project up and running...")
+// app.listen(3000, function(){
+// 	console.log("hp final project up and running...")
+// })
+app.listen( process.env.PORT || 3000, function() {
+  console.log( "hp final project up and running..." )
 })
